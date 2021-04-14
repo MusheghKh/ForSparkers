299ae59646006e712f88491bdefcf98e51329330
Mushegh <asddsajpg@gmail.com>
2021-04-14 21:52:58 +0400

    first commit

---
+++ src/main/java/com/example/forsparkers/ForSparkersApplication.java
@@ -0,0 +1,13 @@
+package com.example.forsparkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForSparkersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForSparkersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forsparkers/ForSparkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forsparkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForSparkersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
